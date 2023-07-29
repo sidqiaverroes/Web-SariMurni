ab32797f543785a23d16e6328cea8b3bf213fb17
sidqiaverroes <sidqi.averroes26@gmail.com>
2023-07-29 15:20:32 +0800

    add hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import NavBar from "@/components/navbar";
+import Hero from "@/components/hero";
 import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <div>
       <NavBar></NavBar>
-      <section id="hero"></section>
+      <Hero></Hero>
       <section id="peta"></section>
       <section id="komoditas"></section>
       <section id="berita"></section>
