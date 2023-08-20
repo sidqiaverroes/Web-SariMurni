60819d786772f2ab816d269cdbf9f75816a8118b
sidqiaverroes <sidqi.averroes26@gmail.com>
2023-08-20 08:48:47 +0700

    edit content in profildesa page

--- src/pages/tentang/profildesa.jsx
+++ src/pages/tentang/profildesa.jsx
@@ -15,34 +15,35 @@ function Profil() {
             <div className="flex flex-col gap-4 w-full">
               <span className=" flex flex-col">
                 <h3 className="text-xl text-c-darkgold">Tentang</h3>
-                <h2 className=" text-c-darkgreen">Desa Banuroja</h2>
+                <h2 className=" text-c-darkgreen">Desa Sari Murni</h2>
               </span>
               <p>
-                Desa Banuroja adalah desa yang terletak di Kecamatan Randangan,
-                Kabupaten Pohuwato, Provinsi Gorontalo. Desa ini memiliki luas
-                wilayah sekitar 10,5 kilometer persegi dan berpenduduk sekitar
-                2.000 jiwa. Desa Banuroja dijuluki sebagai &quot;Desa
-                Pancasila&quot; karena masyarakatnya yang heterogen dan hidup
-                rukun berdampingan, walaupun mereka berasal dari berbagai suku
-                dan agama yang berbeda. Suku-suku yang ada di Desa Banuroja
-                adalah Bali, Nusa Tenggara, Gorontalo, Jawa, dan lainnya. Agama
-                yang ada di Desa Banuroja adalah Islam, Kristen, dan Hindu.
-                Masyarakat Desa Banuroja sangat toleransi satu sama lain, dan
-                mereka selalu menjaga kerukunan antar umat beragama. <br></br>
+                Desa Sari Murni adalah desa yang terletak di Kecamatan
+                Randangan, Kabupaten Pohuwato, Provinsi Gorontalo. Desa ini
+                memiliki luas wilayah sekitar 10,5 kilometer persegi dan
+                berpenduduk sekitar 2.000 jiwa. Desa Sari Murni dijuluki sebagai
+                &quot;Desa Pancasila&quot; karena masyarakatnya yang heterogen
+                dan hidup rukun berdampingan, walaupun mereka berasal dari
+                berbagai suku dan agama yang berbeda. Suku-suku yang ada di Desa
+                Sari Murni adalah Bali, Nusa Tenggara, Gorontalo, Jawa, dan
+                lainnya. Agama yang ada di Desa Sari Murni adalah Islam,
+                Kristen, dan Hindu. Masyarakat Desa Sari Murni sangat toleransi
+                satu sama lain, dan mereka selalu menjaga kerukunan antar umat
+                beragama. <br></br>
                 <br></br>
-                Mata pencaharian utama penduduk Desa Banuroja adalah petani
-                jagung. Selain jagung, penduduk Desa Banuroja juga menanam buah
-                naga, kelapa, dan tanaman lainnya. Masyarakat Desa Banuroja juga
-                beternak sapi, kambing, ayam, dan babi. Desa Banuroja adalah
-                desa yang sangat indah dan nyaman untuk ditinggali.
-                Masyarakatnya yang ramah dan toleransi membuat Desa Banuroja
+                Mata pencaharian utama penduduk Desa Sari Murni adalah petani
+                jagung. Selain jagung, penduduk Desa Sari Murni juga menanam
+                buah naga, kelapa, dan tanaman lainnya. Masyarakat Desa Sari
+                Murni juga beternak sapi, kambing, ayam, dan babi. Desa Sari
+                Murni adalah desa yang sangat indah dan nyaman untuk ditinggali.
+                Masyarakatnya yang ramah dan toleransi membuat Desa Sari Murni
                 menjadi desa yang sangat layak dikunjungi oleh wisatawan dari
                 seluruh Indonesia.
               </p>
             </div>
             <div className="flex overflow-hidden rounded-full w-full h-auto">
               <Image
-                src="/desa.png"
+                src="/desa2.png"
                 alt="img"
                 width={500}
                 height={500}
@@ -97,21 +98,21 @@ function Profil() {
             <div className="flex flex-col justify-center items-center gap-4 ">
               <span className=" flex flex-col items-center text-center">
                 <h3 className="text-xl text-c-gold2">Tentang</h3>
-                <h2 className=" text-white">Logo Desa Banuroja</h2>
+                <h2 className=" text-white">Logo Desa Sari Murni</h2>
               </span>
               <Image
                 src="/logo-banu.png"
-                alt="logo banuroja"
+                alt="logo Sari Murni"
                 width={400}
                 height={400}
               ></Image>
               <p className="text-white text-center">
-                Logo desa banuroja terinspirasi dari burung garuda pancasila
-                yang kemudian diadaptasi untuk mencerminkan Desa Banuroja
+                Logo desa Sari Murni terinspirasi dari burung garuda pancasila
+                yang kemudian diadaptasi untuk mencerminkan Desa Sari Murni
                 sebagai desa pancasila. Sayap dan ekor pada logo membentuk
-                huruf-huruf yang bertuliskan &quot;banuroja&quot;. Bagian logo
-                terbagi menjadi 3 segmen yang merepresentasikan 3 agama di
-                Banuroja, yaitu Islam, Kristen, dan Hindu.
+                huruf-huruf yang bertuliskan &quot;Sari Murni&quot;. Bagian logo
+                terbagi menjadi 3 segmen yang merepresentasikan 3 agama di Sari
+                Murni, yaitu Islam, Kristen, dan Hindu.
               </p>
             </div>
           </section>
@@ -126,22 +127,22 @@ function Profil() {
             <div className="flex flex-col gap-4 w-full">
               <span className=" flex flex-col">
                 <h3 className="text-xl text-c-darkgold">Sejarah</h3>
-                <h2 className=" text-c-darkgreen">Desa Banuroja</h2>
+                <h2 className=" text-c-darkgreen">Desa Sari Murni</h2>
               </span>
               <p>
-                Desa Banuroja didirikan pada tahun 1950 oleh oleh sekelompok
+                Desa Sari Murni didirikan pada tahun 1950 oleh oleh sekelompok
                 orang dari berbagai suku dan agama yang berasal dari Pulau Jawa,
-                Pulau Bali, dan Pulau Lombok. Mereka datang ke Desa Banuroja
+                Pulau Bali, dan Pulau Lombok. Mereka datang ke Desa Sari Murni
                 untuk mencari penghidupan yang lebih baik. <br />
                 <br />
-                Awalnya, Desa Banuroja hanya sebuah desa kecil yang sangat
-                sederhana. Namun, seiring berjalannya waktu, Desa Banuroja
+                Awalnya, Desa Sari Murni hanya sebuah desa kecil yang sangat
+                sederhana. Namun, seiring berjalannya waktu, Desa Sari Murni
                 berkembang menjadi desa yang lebih besar dan lebih maju. Hal ini
-                disebabkan oleh masyarakat Desa Banuroja yang sangat rajin dan
-                gigih bekerja. Masyarakat Desa Banuroja juga sangat toleransi
+                disebabkan oleh masyarakat Desa Sari Murni yang sangat rajin dan
+                gigih bekerja. Masyarakat Desa Sari Murni juga sangat toleransi
                 satu sama lain. Mereka selalu menjaga kerukunan antar umat
                 beragama, walaupun mereka berasal dari berbagai suku dan agama
-                yang berbeda. Hal ini membuat Desa Banuroja menjadi desa yang
+                yang berbeda. Hal ini membuat Desa Sari Murni menjadi desa yang
                 sangat indah dan nyaman untuk ditinggali.
               </p>
             </div>
